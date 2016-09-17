c75afc735927064f4dc7cd808976db8659bd7ed1
Thanabut <mooztubz@gmail.com>
2016-09-17 22:06:07 +0700

    init theme

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'mcvyhzxj_painkillerdev');
+define('DB_NAME', 'mcvyhzxj_painkillerdev1');
 
 /** MySQL database username */
 define('DB_USER', 'mcvyhzxj_wp');
